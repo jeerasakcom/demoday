2b7e3ae37ef3d0c4d32fb8b05c92dca1c6f48a24
topcs032 <6121207032@bsru.ac.th>
2021-02-25 13:57:22 +0700

    _

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,63 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FrontendLayoutComponent } from './layouts/frontend-layout/frontend-layout.component';
+import { HomeComponent } from './pages/frontend/home/home.component';
+import { AboutComponent } from './pages/frontend/about/about.component';
+import { PaymentComponent } from './pages/frontend/payment/payment.component';
+import { ContactComponent } from './pages/frontend/contact/contact.component';
+import { BackendLayoutComponent } from './layouts/backend-layout/backend-layout.component';
+import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/backend/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Route สำหรับเรียกหน้า Frontend Layout
+ {
+   path:'',
+   component: FrontendLayoutComponent,
+   children:[
+     {
+       path: "",
+       component: HomeComponent,
+       pathMatch: 'full'
+     },
+     {
+       path: 'about',
+       component: AboutComponent,
+     },
+     {
+      path: 'payment',
+      component: PaymentComponent,
+    },
+     {
+       path: 'contact',
+       component: ContactComponent,
+     }
+   ]
+ },
+ // Route สำหรับเรียกหน้า Backend Layout
+ {
+   path:'backend',
+   component: BackendLayoutComponent,
+   children:[
+     {
+       path: "",
+       component: DashboardComponent
+     }
+   ]
+ },
+ // Route สำหรับเรียกหน้า Login/Register Layout
+ {
+   path:'login',
+   component: LoginLayoutComponent,
+   children:[
+     {
+       path: "",
+       component: LoginComponent,
+     }
+   ]
+ }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/frontend/home/home.component';
+import { AboutComponent } from './pages/frontend/about/about.component';
+import { ContactComponent } from './pages/frontend/contact/contact.component';
+import { PaymentComponent } from './pages/frontend/payment/payment.component';
+import { DashboardComponent } from './pages/backend/dashboard/dashboard.component';
+import { ReportComponent } from './pages/backend/report/report.component';
+import { StockComponent } from './pages/backend/stock/stock.component';
+import { UsersComponent } from './pages/backend/users/users.component';
+import { SettingComponent } from './pages/backend/setting/setting.component';
+import { LoginComponent } from './pages/login/login.component';
+import { FooterFrontendComponent } from './shared/frontend/footer-frontend/footer-frontend.component';
+import { HeaderFrontendComponent } from './shared/frontend/header-frontend/header-frontend.component';
+import { HeaderBackendComponent } from './shared/backend/header-backend/header-backend.component';
+import { FooterBackendComponent } from './shared/backend/footer-backend/footer-backend.component';
+import { SidebarComponent } from './shared/backend/sidebar/sidebar.component';
+import { BackendLayoutComponent } from './layouts/backend-layout/backend-layout.component';
+import { FrontendLayoutComponent } from './layouts/frontend-layout/frontend-layout.component';
+import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    PaymentComponent,
+    DashboardComponent,
+    ReportComponent,
+    StockComponent,
+    UsersComponent,
+    SettingComponent,
+    LoginComponent,
+    FooterFrontendComponent,
+    HeaderFrontendComponent,
+    HeaderBackendComponent,
+    FooterBackendComponent,
+    SidebarComponent,
+    BackendLayoutComponent,
+    FrontendLayoutComponent,
+    LoginLayoutComponent,
+
   ],
   imports: [
     BrowserModule,
